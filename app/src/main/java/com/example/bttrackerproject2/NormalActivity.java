fb5cc069c543f5fff007c06f35d13e6a0ce30760
RinichiroO <2020m102@kuas.ac.jp>
2020-07-31 22:54:37 +0900

    Class7

--- app/src/main/java/com/example/bttrackerproject2/NormalActivity.java
+++ app/src/main/java/com/example/bttrackerproject2/NormalActivity.java
@@ -2,7 +2,9 @@ package com.example.bttrackerproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NormalActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class NormalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
+    }
+
+    public void normalToHome(View view) {
+        Intent i5 =new Intent(this,MainActivity.class);
+                startActivity (i5);
+
     }
 }
